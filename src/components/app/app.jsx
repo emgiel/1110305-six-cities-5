0077950cd73098daf9ad82746a056b35ac05de47
emgiel <gmemgiel@gmail.com>
2020-10-10 17:40:35 +0300

    Личный проект: маршрут по тайным тропам

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,34 @@
 import React from "react";
-import Main from "../main/main";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import MainScreen from "../main-screen/main-screen";
 import PropTypes from "prop-types";
+import AuthScreen from "../auth-screen/auth-screen";
+import FavoritesScreen from "../favorites-screen/favorites-screen";
+import PropertyScreen from "../property-screen/property-screen";
 
 const App = (props) => {
 
   const {placesCount} = props;
 
   return (
-    <Main placesCount={placesCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainScreen placesCount={placesCount}/>
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/favorites">
+          <FavoritesScreen />
+        </Route>
+        <Route exact path="/property">
+          <PropertyScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
+
 };
 
 App.propTypes = {
